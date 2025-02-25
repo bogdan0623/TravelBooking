91ab721c12a362d40a4b09d32a45a3c2d18067c0
Bogdan Turdean <bogdanturdean@yahoo.com>
2025-02-25 20:12:02 +0200

    Booking models created

---
+++ TravelBooking/Models/DBObjects/Booking.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelBooking.Models.DBObjects
+{
+    public class Booking
+    {
+        public Guid BookingId { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid DestinationId { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public int StatusId { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfPersons { get; set; }
+    }
+}
---
+++ TravelBooking/Models/ViewModels/BookingViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelBooking.Models.ViewModels
+{
+    public class BookingViewModel
+    {
+        public Guid BookingId { get; set; }
+        public CustomerViewModel Customer { get; set; }
+        public DestinationViewModel Destination { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public string Status { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfPersons { get; set; }
+    }
+}
